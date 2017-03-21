b45fd051f73a9e26fdab5b0845eabf77eb01c03f
Reza <hos@eini.com>
2017-03-21 20:12:43 +0100

    add produkt

---
+++ produkt.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class Product{
+	private String name;
+
+	public Produkt()
+	{
+		setName("Coca Cola");
+	}
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+	public void getName()
+	{
+		return name;
+	}
+	
+	}
